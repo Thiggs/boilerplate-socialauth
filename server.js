53d8b79d7d899fa721a5d845a8ee411eca7885ee
Tyler <realpsych22@gmail.com>
2020-04-14 20:38:14 -0500

    passed #2 add github Strategy

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const session     = require('express-session');
 const mongo       = require('mongodb').MongoClient;
 const passport    = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
 
 const app = express();
 
@@ -55,7 +56,16 @@ mongo.connect(process.env.DATABASE, (err, db) => {
         /*
         *  ADD YOUR CODE BELOW
         */
-
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: "https://debonair-vine-gladiolus.glitch.me/auth/github/callback"
+},
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    //Database logic here with callback containing our user object
+  }
+));
               app.route('/auth/github')
           .get(passport.authenticate('github'));
       
